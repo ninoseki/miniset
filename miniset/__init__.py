7211d887278a5ad3798ebc28d9189896e1e3b646
Manabu Niseki <manabu.niseki@gmail.com>
2023-04-09 17:04:08 +0900

    fix: fix package name

--- miniset/__init__.py
+++ miniset/__init__.py
@@ -3,4 +3,4 @@ import importlib.metadata
 from .jinja_context import JinjaTemplateProcessor  # noqa: F401
 from .types import ParamStyleType  # noqa: F401
 
-__version__ = importlib.metadata.version(__name__)
+__version__ = importlib.metadata.version("mini-set")
